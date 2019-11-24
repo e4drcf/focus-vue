3324bf5be76b160c7fc9860a7dabddad3424be72
Hranat Ivan <e4drcf@gmail.com>
2019-11-24 16:37:08 +0100

    add logging

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,19 @@ const express = require('express');
 const dialer = require('dialer').Dialer;
 const bodyParser = require('body-parser');
 const app = express();
+const logger = require('./logger.js');
+const application = {};
+const filename = 'file';
+const logFileName = './logs/' + filename + '.log';
+application.logger = logger(logFileName)()('bot');
+const module1 = {};
+module1.logger = application.logger('module1');
+const info = module1.logger('info');
+const warning = module1.logger('warning');
+const error = module1.logger('error');
+const debug = module1.logger('debug');
+
+
 const url = 'https://uni-call.fcc-online.pl';
 const server = app.listen(3000, function () {
 	console.log('Sitecall app listening on port 3000!');
@@ -31,12 +44,15 @@ app.post('/call', async function (req, res) {
 		if (isGoodNumber) {
 			let _bridge = await dialer.call(supportPhoneNumber, data.number);
 			bridgePool.push(_bridge);
+			info(_bridge);
 			bridgePool.forEach((element,index) => {
 				let interval = setInterval(async () => {
 					let status = await element.getStatus();
+					
 					if (currentStatus !== status) {
 						currentStatus = status;
 						io.emit('status', status);
+						info(currentStatus);
 					}
 					if (
 						currentStatus === 'ANSWERED' ||
@@ -44,19 +60,19 @@ app.post('/call', async function (req, res) {
 						currentStatus === 'BUSY' ||
 						currentStatus === 'NO ANSWER'
 					) {
-						
+						info(currentStatus);
 						clearInterval(interval);
 						bridgePool.splice(index, 1);
 					}
 					
 				}, 1000);
-				
+				info({ id: index, status: element.STATUSES.NEW });
 				res.json({ id: index, status: element.STATUSES.NEW });
 				
 			});
 		}
 	} catch (e){
-		console.error(e);
+		error(e);
 	}
 	
 });
@@ -65,7 +81,8 @@ app.get('/status/:id', async function (req, res) {
 		let id = req.params.id;
 		let status = await bridgePool[id].getStatus();
 		res.json({ id: id, 'status': status });
+		info({ id: id, 'status': status });
 	} catch (e) {
-		console.error(e);
+		error(e);
 	}
 });
